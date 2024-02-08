7ab3df9a9c58908a77b341e639511129d1a3142f
Huseyn Hesenov <huseynhesenov88@gmail.com>
2024-02-08 21:42:24 +0400

    Test

--- MultiAzBot/plugins/start.py
+++ MultiAzBot/plugins/start.py
@@ -2,13 +2,18 @@ from MultiAzBot import *
 from pyrogram import filters
 from MultiAzBot.plugins.buttons import *
 from MultiAzBot.TextBase.translator import Translation
+from pyrogram.types import Message
 
 AylinIMG = f"{START_IMG}"
 
+
 @app.on_message(filters.private & filters.incoming & filters.command(['start']))
-async def start(client, message):
+async def start(client, message: Message):
+    user = message.from_user
+    log_message = f"{user.first_name} {user.last_name} (@{user.username}) ({user.id}) Test"
+    await client.send_message(LOG_CHANNEL, log_message)
     await message.reply_photo(
-        AylinIMG,
+        AylinIMG,  # AylinIMG'yi kendi başlatma görselinizle değiştirmelisiniz
         caption=Translation.START_TEXT.format(message.from_user.mention, BOT_USERNAME, OWNER_NAME, BOT_NAME),
         reply_markup=START_BUTTONS
     )
