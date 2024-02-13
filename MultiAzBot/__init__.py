4a0b421b5b43445d193e1bb253becbe56ece69bd
murrad2003 <158191591+murrad2003@users.noreply.github.com>
2024-02-13 10:51:06 +0400

    Update __init__.py

--- MultiAzBot/__init__.py
+++ MultiAzBot/__init__.py
@@ -4,7 +4,7 @@ from AideTagger_Bot.config import *
 app = Client(
     "AideTagger_Bot",
     api_id=API_ID 28979412,
-    api_hash=API_HASH 0016b3e3a334036da841062673a60f7e,
-    bot_token=BOT_TOKEN 6613265866:AAHBovwUJF57ZHLdh0k71xQqKmNGuzl0PXw,
+    api_hash=API_HASH "0016b3e3a334036da841062673a60f7e",
+    bot_token=BOT_TOKEN "6613265866:AAHBovwUJF57ZHLdh0k71xQqKmNGuzl0PXw",
     plugins=dict(root="AideTagger_Bot.plugins"),
 )
