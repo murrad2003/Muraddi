290dd0386f9f52d2e2cb37851220bb1a4247a009
murrad2003 <158191591+murrad2003@users.noreply.github.com>
2024-02-13 11:08:35 +0400

    Update config.py

--- MultiAzBot/config.py
+++ MultiAzBot/config.py
@@ -6,10 +6,10 @@ BOT_USERNAME = "AideTagger_Bot"
 OWNER_ID = 6617875892
 LOG_CHANNEL = -1001622911009
 PLAYLIST_ID = -1002141488353
-ALIVE_IMG = "https://telegra.ph/file/a0517026e0c4fb49b0a71.jpg"
-START_IMG = "https://telegra.ph/file/a0517026e0c4fb49b0a71.jpg"
+ALIVE_IMG = "https://telegra.ph/file/26e5955b241366d49f92d.jpg"
+START_IMG = "https://telegra.ph/file/26e5955b241366d49f92d.jpg"
 CHANNEL = "AideTagBots"
 SUPPORT = "BizimChat"
 OWNER_NAME = "OldMurad"
-PLAYLIST_NAME = "RikaTagPlaylist"
+PLAYLIST_NAME = "AideTagplaylist"
 MONGODB_URI = "mongodb+srv://RikaTagBot:RikaTagBot@rikatagbot.mkwbpd6.mongodb.net/?retryWrites=true&w=majority"
